cd358d3c0a0bfc82ef6258c0a772348f9863b321
Luis <cookie30.ld@gmail.com>
2021-10-07 00:18:07 -0500

    Cambios en servicio partido - IMPL SW para test

--- Servidor/src/serviciosWeb/ServicioWebPartido.java
+++ Servidor/src/serviciosWeb/ServicioWebPartido.java
@@ -1,12 +1,54 @@
 package serviciosWeb;
 
+import estructural.Partido;
+
+import java.util.List;
+
+import repositorio.JavaServiceJPAPartido;
+
+import servicios.ServicioPartido;
+
 public class ServicioWebPartido {
+    
+    private final ServicioPartido sp;
+    
     public ServicioWebPartido() {
         super();
+        JavaServiceJPAPartido jpa = new JavaServiceJPAPartido();
+        sp = new ServicioPartido(jpa);
     }
     
+    public Integer crearPartido(Partido partido){
+        try{
+            sp.crearPartido(partido);
+        }
+        catch (Exception e){
+            return 1;
+        }
+        return 0;
+    }
     
-    
+    public Integer eliminarPartido(Partido partido){
+        try{
+            sp.eliminarPartido(partido);
+        }
+        catch (Exception e){
+            return 1;
+        }
+        return 0;
+    }
+    public Integer actualizarPartido(Partido partido){
+        try{
+            sp.actualizarPartido(partido);
+        }
+        catch (Exception e){
+            return 1;
+        }
+        return 0;
+    }
+    public List<Partido> listarTodosLosPartidos(){
+        return sp.listarTodosLosPartidos();
+    }
     
     
     
